9f3e8ae7e35b39912bbaca0696dab2518b61569d
Vincent (Leirof) <vince.lrf@gmail.com>
2022-05-09 17:45:06 +0200

    Mode amplitude tentative

--- run.py
+++ run.py
@@ -13,59 +13,71 @@ def gauss(v,m,s):
 
 fmin = config['fmin']
 fgap = config['fgap']
-mods = config['mods']
-tmp = 0
-while tmp < fmin or tmp == 0:
-    tmp += fgap
+Nmods = config['mods']
+def gain_distrib(v):
+    return eval(config['gain_distrib'])
 
-
-v = arange(config['min'], config['max'], config['freq_gap'])
-
-x = linspace(0, config['cavity_lenght'],1000, endpoint=True)
+first_mod = 0
+while first_mod < fmin or first_mod == 0:
+    first_mod += fgap
 
 c = 3e8
+x = linspace(0, c/fgap,1000, endpoint=True)
+v = linspace(fmin, first_mod+Nmods*fgap, 1000, endpoint=True)
 
-mods = arange(100)+1
+mods = arange(Nmods)+1
+mods_freq = arange(first_mod, first_mod+Nmods*fgap, fgap)
+
 periods = []
 phases = []
 amplitudes = []
-tmax = 1
-
 for i in mods:
     periods.append(1/i)#random())
     phases.append(0)#random()*2*pi)
     amplitudes.append(sqrt(i))
 periods = array(periods)
+phases = array(phases)
+amplitudes = array(amplitudes)
 
 t = linspace(0,max(periods)*2,1000)
 
 # waves definition
 def wave(mod,x,t):
     return amplitudes[mod-1]                        \
-        *sin(x/config['cavity_lenght']*pi*mod)      \
+        *sin(x/x[-1]*pi*mod)                        \
         *cos(2*pi*t/periods[mod-1] + phases[mod-1])
 
 # Init plot
-fig, (ax1, ax2) = plt.subplots(2,1)
-ax1.set_ylim(-max(amplitudes[:10]), max(amplitudes[:10]))
-ax2.set_ylim(-1, 1)
-for ax in [ax1,ax2]:
-    ax.set_xlim(0,config['cavity_lenght'])
+fig, (ax2, ax3) = plt.subplots(2,1)
+
+ax2.set_ylim(-max(amplitudes[:10]), max(amplitudes[:10]))
+ax3.set_ylim(-1, 1)
+for ax in [ax2,ax3]:
+    ax.set_xlim(x[0],x[-1])
     ax.grid()
 
+
 # First plot at t=0
+
+# modlines = ax1.bar(mods_freq, amplitudes, width=fgap/10, color='b')
+# ax1.plot(v,gain_distrib(v))
+
 total = zeros(len(x))
 lines = []
 for i in mods:
     w = wave(i,x,0)
-    if i<10: lines.append(ax1.plot(x,w,label=f"mod {i}")[0])
+    if i<10: lines.append(ax2.plot(x,w,label=f"mod {i}")[0])
     total += w
-line_tot, = ax2.plot(x, total)
+line_tot, = ax3.plot(x, total)
+
 
 # Plotting for a given time
 def run(T):
+    global amplitudes
     total = zeros(len(x))
+    amplitudes *= gain_distrib(mods_freq)
     for i in mods:
+        # modlines[i-1].set_height(amplitudes[i-1])
         w = wave(i,x,T)
         if i<10: lines[i-1].set_data(x,w)
         total += w
