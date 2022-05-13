1e5ce6e74dd069fb7151d68887762fc285151482
Vincent (Leirof) <vince.lrf@gmail.com>
2022-05-13 16:39:15 +0200

    Improvements

--- run.py
+++ run.py
@@ -9,54 +9,62 @@ with open("config.yml", 'r') as stream:
     print(config)
 
 def gauss(v,m,s):
-    return 1/(s*sqrt(2*pi))*exp(-0.5*((v-m)/s)**2)  # gaussian distribution
+    return exp(-0.5*((v-m)/s)**2) #1/(s*sqrt(2*pi))*  # gaussian distribution
+
+def gain_distrib(v):
+    global config
+    return eval(config['gain_distrib'])
 
 fmin = config['fmin']
 fgap = config['fgap']
 mods = config['mods']
+c = 3e8
+cavity_lenght = c/fgap
+
 tmp = 0
 while tmp < fmin or tmp == 0:
     tmp += fgap
 
 
-v = arange(config['min'], config['max'], config['freq_gap'])
+v = arange(fmin, fmin+fgap*mods, fgap)
+x = linspace(0, cavity_lenght,1000, endpoint=True)
 
-x = linspace(0, config['cavity_lenght'],1000, endpoint=True)
-
-c = 3e8
-
-mods = arange(100)+1
 periods = []
 phases = []
 amplitudes = []
 tmax = 1
 
-for i in mods:
+for i in arange(mods)+1:
     periods.append(1/i)#random())
     phases.append(0)#random()*2*pi)
-    amplitudes.append(sqrt(i))
+    amplitudes.append(1)#sqrt(i))
 periods = array(periods)
+amplitudes = array(amplitudes)
+amplitudes = amplitudes/max(amplitudes)
 
 t = linspace(0,max(periods)*2,1000)
 
 # waves definition
 def wave(mod,x,t):
     return amplitudes[mod-1]                        \
-        *sin(x/config['cavity_lenght']*pi*mod)      \
+        *sin(x/cavity_lenght*pi*mod)                \
         *cos(2*pi*t/periods[mod-1] + phases[mod-1])
 
 # Init plot
-fig, (ax1, ax2) = plt.subplots(2,1)
+fig, (ax3, ax1, ax2) = plt.subplots(3,1)
 ax1.set_ylim(-max(amplitudes[:10]), max(amplitudes[:10]))
 ax2.set_ylim(-1, 1)
 for ax in [ax1,ax2]:
-    ax.set_xlim(0,config['cavity_lenght'])
+    ax.set_xlim(0,cavity_lenght)
     ax.grid()
+
+ax3.bar(v,amplitudes)
+ax3.plot(v,gain_distrib(v),'r--')
 
 # First plot at t=0
 total = zeros(len(x))
 lines = []
-for i in mods:
+for i in arange(mods)+1:
     w = wave(i,x,0)
     if i<10: lines.append(ax1.plot(x,w,label=f"mod {i}")[0])
     total += w
@@ -65,7 +73,7 @@ line_tot, = ax2.plot(x, total)
 # Plotting for a given time
 def run(T):
     total = zeros(len(x))
-    for i in mods:
+    for i in arange(mods)+1:
         w = wave(i,x,T)
         if i<10: lines[i-1].set_data(x,w)
         total += w
