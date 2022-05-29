ab1aeb0b5b2fa69bb6cdb8ee0a425d5914aa9402
leirof <vince.lrf@gmail.com>
2022-05-29 13:38:42 +0200

    I think the code is done. Let's start the report!

--- run.py
+++ run.py
@@ -1,119 +1,185 @@
-from cProfile import label
-from tkinter import Label
 import yaml
 from numpy import *
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from random import random
 
+"""
+  _   _ _   _ _     
+ | | | | |_(_) |___ 
+ | | | | __| | / __|
+ | |_| | |_| | \__ \
+  \___/ \__|_|_|___/
+                    
+"""
+
 with open("config.yml", 'r') as stream:
     config=yaml.safe_load(stream)
-    print(config)
+    print("⚙️ Configuration:")
+    for key, value in config.items(): print(f"   {key}: {value}")
 
 def gauss(v,m,s):
-    return exp(-0.5*((v-m)/s)**2) #1/(s*sqrt(2*pi))*  # gaussian distribution
+    return exp(-0.5*((v-m)/s)**2)  # gaussian distribution wth max value = 1
 
-def gain_distrib(v):
-    global config
-    return eval(config['gain_distrib'])
+# waves definition
+def wave(mode,x,t):
+    return (
+        amplitudes[mode]                          # Amplitude
+        *sin(x/cavity_lenght*pi*mode)             # Space dependence
+        *exp(-1j*(t*2*pi*v[mode]+phases[mode]))   # Time dependence
+    )
 
-fmin = config['fmin']
-fgap = config['fgap']
-modes = config['modes']
+
+"""
+  ___       _ _   _       _ _          _   _             
+ |_ _|_ __ (_) |_(_) __ _| (_)______ _| |_(_) ___  _ __  
+  | || '_ \| | __| |/ _` | | |_  / _` | __| |/ _ \| '_ \ 
+  | || | | | | |_| | (_| | | |/ / (_| | |_| | (_) | | | |
+ |___|_| |_|_|\__|_|\__,_|_|_/___\__,_|\__|_|\___/|_| |_|
+                                                         
+"""
+
+def period_distrib(i,v):    return eval(config['period_distrib'   ])
+def phase_distrib(i,v):     return eval(config['phase_distrib'    ])
+def amplitude_distrib(i,v): return eval(config['amplitude_distrib'])
+def gain_distrib(i,v):      return eval(config['gain_distrib'     ])
+
+fgap    = config['fgap']
+modes   = config['modes']
 dispMax = min(modes,config['display_modes'])
-c = 3e8
+
+c             = 3e8
 cavity_lenght = c/fgap
 
-tmp = 0
-while tmp < fmin or tmp == 0:
-    tmp += fgap
-
-
-v = arange(fmin, fmin+fgap*modes, fgap)
+v = arange(fgap, fgap*(modes+1), fgap)
 x = linspace(0, cavity_lenght,1000, endpoint=True)
 
-periods = []
-phases = []
+periods    = []
+phases     = []
 amplitudes = []
-tmax = 1
+gain       = []
 
-for i in arange(modes)+1:
-    periods.append(v[i-1])#/i)#random())
-    phases.append(random()*2*pi)
-    amplitudes.append(1)#sqrt(i))
-periods = array(periods)
-amplitudes = array(amplitudes)
-amplitudes = amplitudes/max(amplitudes)
-phases = array(phases)
+for i in arange(modes):
+    periods.append(period_distrib(i+1, v[i]))
+    phases.append(phase_distrib(i+1, v[i]))
+    amplitudes.append(amplitude_distrib(i+1, v[i]))
+    gain.append(gain_distrib(i+1, v[i]))
+
+periods                  = array(periods)
+amplitudes               = array(amplitudes)
+amplitudes               = amplitudes/max(amplitudes)
+phases                   = array(phases)
+gain                     = array(gain)
+gain[gain > max(gain)/2] = max(gain)/2
+
+initial_amplitudes = copy(amplitudes)
+amplitudes *= gain
 
 t = linspace(0,max(periods)*2,1000)
 
-# waves definition
-def wave(mod,x,t):
-    return amplitudes[mod-1]                        \
-        *sin(x/cavity_lenght*pi*mod)                \
-        *cos(2*pi*t/periods[mod-1] + phases[mod-1])
+print("🧮 Computing...")
+total_evol = []
+waves_evol = []
+for i, T in enumerate(t):
+    waves_evol.append([])
+    total = zeros(len(x)).astype(complex)
 
+    for m in arange(modes):
+        w = wave(m,x,T)
+        waves_evol[i].append(w)
+        total += w
+    total_evol.append(total)
+
+waves_evol = array(waves_evol)
+total_evol = array(total_evol)
+
+real_max = amax(real(total_evol))
+abs_max = amax(abs(total_evol))
+print("  -> Maximum pulse intensity :",round(abs_max**2,2))
+
+"""
+  ____                        __ _          _      __                          
+ |  _ \ _ __ __ ___      __  / _(_)_ __ ___| |_   / _|_ __ __ _ _ __ ___   ___ 
+ | | | | '__/ _` \ \ /\ / / | |_| | '__/ __| __| | |_| '__/ _` | '_ ` _ \ / _ \
+ | |_| | | | (_| |\ V  V /  |  _| | |  \__ \ |_  |  _| | | (_| | | | | | |  __/
+ |____/|_|  \__,_| \_/\_/   |_| |_|_|  |___/\__| |_| |_|  \__,_|_| |_| |_|\___|
+                                                                               
+"""
+
+# __________________________________________________
 # Init plot
-fig, (ax1, ax2, ax3) = plt.subplots(3,1)
+fig, (ax1, ax2, ax3) = plt.subplots(3,1,figsize=(16,9))
 
-
-# ax1.vlines(x=v,ymin=0,ymax=amplitudes/max(amplitudes))
+# __________________________________________________
+# First subplot : input data
 lines1 = []
-for i in v: ax1.axvline(i, color='k', linestyle='--')
-lines1.append(ax1.plot(  v,  gain_distrib(v) /max(gain_distrib(v)),  c='0.55',  label=f"Laser gain (*{round(max(gain_distrib(v)),3)})"    ))
-lines1.append(ax1.plot(  v,  amplitudes      /max(amplitudes),       'y',       label=f"Amplitudes (*{round(max(amplitudes),3)})"        ))
-lines1.append(ax1.plot(  v,  phases%(2*pi)   /(2*pi),                'g',       label='Phases (*2*Pi)'                                  ))
-lines1.append(ax1.plot(  v,  periods         /max(abs(periods)),     'b',       label=f"Periods (*{round(max(abs(periods)),3)})"         ))
+lines1.append(ax1.plot(  v,  gain /max(gain),  c='0.55',  label=f"Laser gain (*{round(max(gain),3)})"    ))
+ax1.vlines(v[0],ymin=0,ymax=amplitudes[0]/max(amplitudes), color='r', label=f"Modes (*{round(max(amplitudes),3)})")
+for i in range(modes)[1:]: ax1.vlines(v[i],ymin=0,ymax=amplitudes[i]/max(amplitudes), color='r')
+lines1.append(ax1.plot(  v,  initial_amplitudes   /max(initial_amplitudes), 'y',       label=f"Initial modes amplitudes (*{round(max(initial_amplitudes),3)})" ))
+lines1.append(ax1.plot(  v,  phases%(2*pi)        /(2*pi),                  'g',       label='Phases (*2*Pi)'                                                  ))
+lines1.append(ax1.plot(  v,  periods              /max(abs(periods)),       'b',       label=f"Periods (*{round(max(abs(periods)),3)})"                        ))
 
-ax1.set_ylim(0,1)
+#  Plot properties
+ax1.set_ylim(ymin=0)
 ax1.grid()
 ax1.legend()
 ax1.set_title("Laser parameters",loc='left')
 ax1.set_xlabel("Frequency (Hz)")
 ax1.set_ylabel("Normalized Gain/Amplitude/Phase/Period")
 
-total = zeros(len(x))
+# __________________________________________________
+# Second and third subplot : waves and sum
+total = zeros(len(x)).astype(complex)
 lines = []
-for i in arange(modes)+1:
-    w = wave(i,x,0)
-    if i<=dispMax: lines.append(ax2.plot(x,w,label=f"mod {i}")[0])
-    total += w
-line_tot, = ax3.plot(x, zeros(len(x)), label="Total")
+lines_tot = []
+for i in arange(modes):
+    w = waves_evol[0][i]
+    if i < dispMax: lines.append(ax2.plot(x,real(w)/max(amplitudes[:dispMax]),label=f"mode {i}")[0]) # Plot the 10 first modes
+total = total_evol[0]
+lines_tot.append(ax3.plot(x, real(total)/real_max,    label=f"Amplitude (real part) (*{round(real_max,2)})")[0])
+lines_tot.append(ax3.plot(x, (abs(total)/abs_max)**2, label=f"Instensity (*{round(real_max**2,2)})")[0])
 
-ax2.set_ylim(-max(amplitudes[:dispMax]), max(amplitudes[:dispMax]))
-ax3.set_ylim(-sum(amplitudes), sum(amplitudes))
+# Plot properties
 ax2.set_title("Laser waves",loc='left')
 ax3.set_title("Sum of waves",loc='left')
-ax2.set_ylim(-max(amplitudes[:dispMax]), max(amplitudes[:dispMax]))
+ax3.legend()
+ax2.set_ylabel(f"Normalized amplitude (*{round(max(amplitudes[:dispMax]),2)})")
+ax3.set_ylabel("Normalized amplitude")
 for ax in [ax2,ax3]:
+    ax.set_ylim(-1,1)
     ax.set_xlim(0,cavity_lenght)
     ax.grid()
     ax.set_xlabel("Distance (m)")
-    ax.set_ylabel("Amplitude")
 
+"""
+     _          _                 _   _             
+    / \   _ __ (_)_ __ ___   __ _| |_(_) ___  _ __  
+   / _ \ | '_ \| | '_ ` _ \ / _` | __| |/ _ \| '_ \ 
+  / ___ \| | | | | | | | | | (_| | |_| | (_) | | | |
+ /_/   \_\_| |_|_|_| |_| |_|\__,_|\__|_|\___/|_| |_|
+                                                    
+"""
 
-im3, = ax3.plot([], [], color=(0,0,1))
-
+# __________________________________________________
 # Plotting for a given time
 def run(T):
     global amplitudes
-    print(f"Progress: {round(T/t[-1]*100,2)} %",end="\r")
+    print(f"🎞️ Generating animation... {round((T+1)/len(t)*100,2)} %",end="\r")
 
-    total = zeros(len(x))
+    total = zeros(len(x)).astype(complex)
     
-    for i in arange(modes)+1:
-        w = wave(i,x,T)
-        total += w
+    for i in arange(modes):
+        if i < dispMax: lines[i].set_data(x,real(waves_evol[T][i])/max(amplitudes[:dispMax]))
+    
+    total = total_evol[T]
+    lines_tot[0].set_data(x,(real(total)/real_max))
+    lines_tot[1].set_data(x,(abs(total)/abs_max)**2)
 
-        if i<=dispMax:
-            lines[i-1].set_data(x,w)
-    line_tot.set_data(x,total)
+    return lines + lines_tot
 
-    return lines + [line_tot]
+ani = animation.FuncAnimation(fig, run, arange(len(t)), blit=True, interval=10, repeat=True)
 
-ani = animation.FuncAnimation(fig, run, t, blit=True, interval=10,
-    repeat=True)
-
-ani.save(f"result.mp4")
+try: ani.save(f"result.mp4")
+except: print("⚠️ [WARNING] Cannot save the file. Please install ffmpeg to save the animation as a video. https://ffmpeg.org")
 plt.show()
